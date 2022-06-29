800bee4ba71d3c5ea8b4033127c04bc3f55b5757
razi16 <99330297+razi16@users.noreply.github.com>
2022-06-29 16:32:29 +0700

    Update about-us.js
    Updating all of the image element src attribute.

--- src/scripts/views/pages/about-us.js
+++ src/scripts/views/pages/about-us.js
@@ -3,7 +3,7 @@ const AboutUs = {
     return `
     <article>
     <section class="teamMember" tabindex="0">
-        <img src="../images/image1.jpg" alt="Ahmad Razi"/>
+        <img src="images/image1.jpg" alt="Ahmad Razi"/>
         <p style="font-weight: bold;" tabindex="0">Ahmad Razi</p>
         <p tabindex="0">Mahasiswa jurusan Sistem Informasi dari UPN Veteran Jakarta, akhir - akhir ini saya tertarik dengan web development dan saya sudah 
             mencoba belajar menggunakan html, css dan sedikit JavaScript</p>
@@ -13,7 +13,7 @@ const AboutUs = {
     </section>
     
     <section class="teamMember" tabindex="0">
-        <img src="../images/image2.jpg" alt="Ela Amelia Sari"/>
+        <img src="images/image2.jpg" alt="Ela Amelia Sari"/>
         <p style="font-weight: bold;" tabindex="0">Ela Amelia Sari</p>
         <p tabindex="0">Mahasiswa semester 6 program studi teknik informatika dari Universitas Singaperbangsa Karawang,
                  berasal dari kota Subang-Jawa Barat.
@@ -24,7 +24,7 @@ const AboutUs = {
     </section>
     
     <section class="teamMember" tabindex="0">
-        <img src="../images/image3.jpg" alt="Muhammad Raihan Hatim"/>
+        <img src="images/image3.jpg" alt="Muhammad Raihan Hatim"/>
         <p style="font-weight: bold;" tabindex="0">Muhammad Raihan Hatim</p>
         <p tabindex="0">Seorang Mahasiswa Universitas Komputer Indonesia Semester 6 Jurusan Teknik Informatika. Berasal Dari Jakarta Utara</p>
         <p tabindex="0">Kesibukan: Saat ini sedang kuliah dan mengikuti SIB Dicoding.</p>
@@ -33,7 +33,7 @@ const AboutUs = {
     </section>
     
     <section class="teamMember" style="margin-bottom: 75px;"tabindex="0">
-        <img src="../images/image4.jpg" alt="R. Ramdhani Purbowo"/>
+        <img src="images/image4.jpg" alt="R. Ramdhani Purbowo"/>
         <p style="font-weight: bold;" tabindex="0">R. Ramdhani Purbowo</p>
         <p tabindex="0">Seorang mahasiswa semester 6 di STMIK Amik Bandung, tinggal di Kota Bandung.</p>
         <p tabindex="0">Kesibukan: Saat ini sedang program SIB Dicoding Academy Batch 2.</p>
