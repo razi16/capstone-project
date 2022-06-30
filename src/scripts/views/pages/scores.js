6594cb2639011bff2aecd45e9e6a087b746d5b1a
razi16 <99330297+razi16@users.noreply.github.com>
2022-06-30 13:54:31 +0700

    Update scores.js
    Adding tab index to this page

--- src/scripts/views/pages/scores.js
+++ src/scripts/views/pages/scores.js
@@ -10,13 +10,14 @@ const Scores = {
 
   async afterRender() {
     const scoreContainer = document.getElementById('score-container');
-    scoreContainer.innerHTML = '<p>The last 5 scores will be displayed here</p>';
+    scoreContainer.innerHTML = '<p tabindex="0">The last 5 scores will be displayed here</p>';
     const serializedData = localStorage.getItem(STORAGE_KEY);
     const data = JSON.parse(serializedData);
 
     if (data !== null) {
       for (const score of data) {
         const scoreElement = document.createElement('p');
+        scoreElement.setAttribute('tabindex', '0');
         scoreElement.innerText = `${score.score} (${score.time})`;
         scoreContainer.append(scoreElement);
       }
